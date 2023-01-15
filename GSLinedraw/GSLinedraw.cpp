1d7951b53775bebd7a2122245f5e4efe31a937b8
GoldenStarGamer <vicentetavareslima@gmail.com>
2023-01-15 01:03:13 +0000

    added demo.

--- GSLinedraw/GSLinedraw.cpp
+++ GSLinedraw/GSLinedraw.cpp
@@ -2,9 +2,19 @@
 #include "cinc.h"
 #include "Linedraw.h"
 
+float povx, povy, tx, ty;
+
 int main()
 {
+	cout << "your x coord.:";
+	cin >> povx;
+	cout << "\nyour y coord.:";
+	cin >> povy;
+	cout << "\ntarget's x coord.:";
+	cin >> tx;
+	cout << "\ntarget's y coord.:";
+	cin >> ty;
 	Linedraw ld;
-	cout << ld.Dist(0, 1, 1, 2);
+	cout << ld.Dist(0, -1, 1, 2);
 	return 0;
 }
